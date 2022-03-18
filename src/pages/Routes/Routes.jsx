bf8dded9d53a4473f7bff9a0c182bbfdff2d6aa5
colaMeowSY <stephy99y@gmail.com>
2022-03-18 19:30:12 +1300

    Organise Chat Page
    -Make components responsive to resize

--- src/pages/Routes/Routes.jsx
+++ src/pages/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes as Switch } from 'react-router-dom';
 import Example from '../../components/Example';
 import Nav from '../../components/NavBar';
 import Dashboard from '../Dashboard';
+import Chat from '../Chat/Chat';
 
 function Routes() {
   return (
@@ -11,7 +12,7 @@ function Routes() {
       <Switch>
         <Route path="/courses" element={<Example />} />
         <Route path="/find-matches" element={<Example />} />
-        <Route path="/chat" element={<Example />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="/accounts" element={<Example />} />
         <Route path="/" element={<Dashboard />} />
       </Switch>
